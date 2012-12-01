e828d206b4a76abe31f90c4327cfc46e7b475a33
Sean Pilkenton <seanpilk@Seans-MacBook-Pro.local>
2012-12-01 10:54:40 -0500

    Initial Commit

---
+++ Quarrels/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Quarrels
+//
+//  Created by Sean Pilkenton on 12/1/12.
+//  Copyright (c) 2012 Near Infinity. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
